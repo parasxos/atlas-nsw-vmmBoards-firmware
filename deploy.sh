8545ffe0b668fe33e99ea88bd183310955326db2
Paris Moschovakos <paris.moschovakos@cern.ch>
2017-03-28 18:42:54 +0200

    WIP: Changed deploy to accomodate new board type

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ echo "Cleaning up the build directory..."
 cd sources
 rm -Rf MDT_446 2>&1 >/dev/null
 rm -Rf MDT_MU2E 2>&1 >/dev/null
+rm -Rf MMMFE8_VMM3 2>&1 >/dev/null
 echo "Buildind project..."
-vivado -mode batch -nolog -nojournal -notrace -source build_mmfe8.tcl -tclargs mdt_446 2>&1 >/dev/null
+vivado -mode batch -nolog -nojournal -notrace -source build_mmfe8.tcl -tclargs mmfe8_vmm3 2>&1 >/dev/null
 echo "Build was made in project directory. Starting project..."
-cd MDT_446/
-vivado MDT_446.xpr
+cd MMFE8_VMM3/
+vivado MMFE8_VMM3.xpr

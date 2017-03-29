e23bc1152dab6b0fad72fa1baa74a7524c455734
Paris Moschovakos <paris.moschovakos@cern.ch>
2017-03-29 17:41:07 +0200

    Updated deploy.sh for multiple board types

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,30 @@ cd sources
 rm -Rf MDT_446 2>&1 >/dev/null
 rm -Rf MDT_MU2E 2>&1 >/dev/null
 rm -Rf MMMFE8_VMM3 2>&1 >/dev/null
-echo "Buildind project..."
-vivado -mode batch -nolog -nojournal -notrace -source build_mmfe8.tcl -tclargs mmfe8_vmm3 2>&1 >/dev/null
+
+tclArg=$1
+projectDir="MMFE8_VMM3/"
+projectXpr="MMFE8_VMM3.xpr"
+
+if [ "$1" == "mmfe8_vmm3" ] 
+then
+	echo "Buildind project for:" $1
+elif [ "$1" == "mdt_446" ] 
+then
+	echo "Buildind project for:" $1
+	projectDir="MDT_446/"
+	projectXpr="MDT_446.xpr"
+elif [ "$1" == "mdt_mu2e" ] 
+then
+	echo "Buildind project for:" $1
+	projectDir="MDT_MU2E/"
+	projectXpr="MDT_MU2E.xpr"
+else
+	tclArg="mmfe_vmm3"
+	echo "Building project for:" $tclArg	
+fi
+
+vivado -mode batch -nolog -nojournal -notrace -source build_mmfe8.tcl -tclargs $tclArg 2>&1 >/dev/null
 echo "Build was made in project directory. Starting project..."
-cd MMFE8_VMM3/
-vivado MMFE8_VMM3.xpr
+cd $projectDir
+vivado $projectXpr 

35ac537563d3fa6754d817cf0f89607fd9a6e198
Christos Bakalis <christos.bakalis@cern.ch>
2017-04-25 12:16:46 +0200

    Added udp reply handling module

--- sources/build_mmfe8.tcl
+++ sources/build_mmfe8.tcl
@@ -150,6 +150,7 @@ set obj [get_filesets sources_1]
 set files [list \
  "[file normalize "$origin_dir/sources_1/mmfe8_top.vhd"]"\
  "[file normalize "$origin_dir/sources_1/configuration/udp_data_in_handler.vhd"]"\
+ "[file normalize "$origin_dir/sources_1/configuration/udp_reply_handler.vhd"]"\
  "[file normalize "$origin_dir/sources_1/configuration/fpga_config_block.vhd"]"\
  "[file normalize "$origin_dir/sources_1/configuration/vmm_config_block.vhd"]"\
  "[file normalize "$origin_dir/sources_1/configuration/axi_quad_top.vhd"]"\
@@ -238,6 +239,11 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property "file_type" "VHDL" $file_obj
 
 set file "$origin_dir/sources_1/configuration/udp_data_in_handler.vhd"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property "file_type" "VHDL" $file_obj
+
+set file "$origin_dir/sources_1/configuration/udp_reply_handler.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property "file_type" "VHDL" $file_obj

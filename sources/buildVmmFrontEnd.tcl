6f56dcad75c6bfa117ef6b936f64eb6b2acfccd4
Christos Bakalis <christos.bakalis@cern.ch>
2017-07-17 12:49:01 +0200

    Added GPL license

--- sources/buildVmmFrontEnd.tcl
+++ sources/buildVmmFrontEnd.tcl
@@ -711,4 +711,21 @@ set_property "steps.write_bitstream.args.verbose" "0" $obj
 # set the current impl run
 current_run -implementation [get_runs impl_1]
 
+puts "Copyright Notice/Copying Permission:
+    Copyright 2017 Paris Moschovakos, Panagiotis Gkountoumis & Christos Bakalis\n
+
+    This file is part of NTUA-BNL_VMM_firmware.\n
+
+    NTUA-BNL_VMM_firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NTUA-BNL_VMM_firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NTUA-BNL_VMM_firmware.  If not, see <http://www.gnu.org/licenses/>.\n"
 puts "INFO: Project created:$projectname"
--- sources/sources_1/readout/vmm_driver.vhd
+++ sources/sources_1/readout/vmm_driver.vhd
@@ -2,6 +2,24 @@
 -- Company: NTU Athens - BNL
 -- Engineer: Christos Bakalis (christos.bakalis@cern.ch) 
 -- 
+-- Copyright Notice/Copying Permission:
+--    Copyright 2017 Christos Bakalis
+--
+--    This file is part of NTUA-BNL_VMM_firmware.
+--
+--    NTUA-BNL_VMM_firmware is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    NTUA-BNL_VMM_firmware is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with NTUA-BNL_VMM_firmware.  If not, see <http://www.gnu.org/licenses/>.
+-- 
 -- Create Date: 25.04.2017 17:45:32
 -- Design Name: VMM Driver
 -- Module Name: vmm_driver - RTL
